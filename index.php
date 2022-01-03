b68663575f845228a09a35099310bc528df61bb1
DhanushaSuharshana <dinudhanusha@gmail.com>
2022-01-03 12:34:35 +0530

    #2 changes

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <meta name="description" content="Directro - Directory and Listing Template" />
     <meta name="author" content="potenzaglobalsolutions.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Directro - Directory and Listing Template</title>
-
+    <title>Directory and Listing</title>
     <!-- Favicon -->
     <link rel="shortcut icon" href="images/favicon.ico" />
 
